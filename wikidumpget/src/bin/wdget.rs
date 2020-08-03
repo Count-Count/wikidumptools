d815531c08e62f8bfe40c66652ef676fc68ad2a5
Count Count <countvoncount123456@gmail.com>
2020-08-03 23:33:54 +0200

    wdget WIP

--- wikidumpget/src/bin/wdget.rs
+++ wikidumpget/src/bin/wdget.rs
@@ -5,11 +5,8 @@
 // Distributed under the terms of the MIT license.
 
 use atty;
-use clap::{App, Arg};
-use regex::RegexBuilder;
+use clap::{App, AppSettings, Arg};
 use reqwest::Client;
-use std::collections::BTreeSet;
-use std::iter::FromIterator;
 use termcolor::ColorChoice;
 use thiserror::Error;
 
@@ -19,13 +16,15 @@ pub enum WDGetError {
     HttpError(#[from] reqwest::Error),
     #[error("Error parsing JSON: {0}")]
     JsonError(#[from] serde_json::Error),
-    #[error("Error in JSON structure")]
-    JsonStructureError(),
+    #[error("Received invalid JSON data from Wikidata.")]
+    InvalidJsonFromWikidata(),
+    #[error("Dump status JSON data is invalid.")]
+    InvalidJsonFromDumpStatus(),
 }
 
 type Result<T> = std::result::Result<T, WDGetError>;
 
-async fn get_available_wikis_from_wikidata(client: &Client) -> Result<Vec<String>> {
+async fn get_available_wikis_from_wikidata() -> Result<Vec<String>> {
     let mut wikis = Vec::with_capacity(50);
     let sparql_url = "https://query.wikidata.org/sparql";
     let query = "
@@ -34,6 +33,7 @@ async fn get_available_wikis_from_wikidata(client: &Client) -> Result<Vec<String
         FILTER(NOT EXISTS { ?item wdt:P31 wd:Q33120923. })
       }
       ";
+    let blacklist = ["ecwikimedia", "labswiki", "labtestwiki", "ukwikiversity"];
     let client = reqwest::Client::builder().user_agent("wdget/0.1").build()?;
     let r = client
         .get(sparql_url)
@@ -43,59 +43,105 @@ async fn get_available_wikis_from_wikidata(client: &Client) -> Result<Vec<String
         .error_for_status()?;
     let body = r.text().await?;
     let json: serde_json::Value = serde_json::from_str(body.as_str())?;
-    let x = json.get("results").unwrap();
     if let serde_json::Value::Array(vec) = &json["results"]["bindings"] {
         for entry in vec {
             if let serde_json::Value::String(s) = &entry["id"]["value"] {
-                wikis.push(s.to_owned());
+                if !blacklist.contains(&s.as_str()) {
+                    wikis.push(s.to_owned());
+                }
             } else {
-                return Err(WDGetError::JsonStructureError());
+                return Err(WDGetError::InvalidJsonFromWikidata());
             }
         }
     } else {
-        return Err(WDGetError::JsonStructureError());
+        return Err(WDGetError::InvalidJsonFromWikidata());
     }
     Ok(wikis)
 }
 
-async fn get_available_wikis_dumps(client: &Client) -> Result<Vec<String>> {
-    let mut wikis = Vec::with_capacity(50);
-    let res = client
-        .get("https://dumps.wikimedia.org/backup-index.html")
-        .send()
-        .await?
-        .error_for_status()?;
-    let body = res.text().await?;
-    let regex = RegexBuilder::new(r#"<a href="([^/]+)/[0-9]{8}">"#)
-        .build()
-        .unwrap();
-    for caps in regex.captures_iter(&body) {
-        wikis.push(caps.get(1).unwrap().as_str().to_owned());
+async fn list_wikis() -> Result<()> {
+    let mut wikis = get_available_wikis_from_wikidata().await.unwrap();
+    wikis.sort();
+    for wiki in wikis {
+        println!("{}", wiki);
     }
-    Ok(wikis)
+    Ok(())
 }
 
-async fn runx() -> Result<()> {
-    let res = reqwest::get("http://httpbin.orgx/get").await?;
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
+fn create_client() -> Result<Client> {
+    Ok(reqwest::Client::builder().user_agent("wdget/0.1").build()?)
+}
 
-    let body = res.text().await?;
-    println!("Body:\n{}", body);
+async fn list_types(wiki: &str, date: &str) -> Result<()> {
+    let client = reqwest::Client::builder().user_agent("wdget/0.1").build()?;
+    let url = format!(
+        "https://dumps.wikimedia.org/{}/{}/dumpstatus.json",
+        wiki, date
+    );
+    let r = client.get(url.as_str()).send().await?.error_for_status()?;
+    let body = r.text().await?;
+    let json: serde_json::Value = serde_json::from_str(body.as_str())?;
+    if let serde_json::Value::Object(map) = &json["jobs"] {
+        for key in map.keys() {
+            println!("{}", key)
+        }
+    } else {
+        return Err(WDGetError::InvalidJsonFromDumpStatus());
+    }
     Ok(())
 }
 
 async fn run() -> Result<()> {
+    let wiki_name_arg = Arg::with_name("wiki name")
+        .help("Name of the wiki")
+        .required(true);
+    let dump_date_arg = Arg::with_name("dump date")
+        .help("Date of the dump (YYYYMMDD)")
+        .required(true);
+
     let matches = App::new("wikidumget")
         .version("0.1")
         .author("Count Count <countvoncount123456@gmail.com>")
         .about("Download Wikipedia dumps from the internet.")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::DeriveDisplayOrder)
         .arg(
             Arg::with_name("verbose")
                 .short("v")
                 .long("verbose")
-                .help("print performance statistics"),
+                .help("Print performance statistics"),
         )
+        .subcommand(
+            App::new("download")
+                .about("Download a wiki dump")
+                .arg(wiki_name_arg.clone())
+                .arg(dump_date_arg.clone())
+                .arg(
+                    Arg::with_name("dump type")
+                        .help("Type of the dump")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("mirror")
+                        .long("mirror")
+                        .help("Mirror to use")
+                        .takes_value(true)
+                        .max_values(1),
+                ),
+        )
+        .subcommand(App::new("list-wikis").about("List all wikis for which dumps are available"))
+        .subcommand(
+            App::new("list-dates")
+                .about("List all dump dates available for this wiki")
+                .arg(wiki_name_arg.clone()),
+        )
+        .subcommand(
+            App::new("list-types")
+                .about("List all types available in this dump")
+                .arg(wiki_name_arg.clone())
+                .arg(dump_date_arg),
+        )
+        .subcommand(App::new("list-mirrors").about("List avaliable mirrors"))
         .get_matches();
 
     let color_choice = if atty::is(atty::Stream::Stdout) {
@@ -103,19 +149,18 @@ async fn run() -> Result<()> {
     } else {
         ColorChoice::Never
     };
-    let client = reqwest::Client::builder().user_agent("wdget/0.1").build()?;
-    let wdwikis = get_available_wikis_from_wikidata(&client).await.unwrap();
-    let wdwikis_set: BTreeSet<String> = BTreeSet::from_iter(wdwikis);
-    let dumpwikis = get_available_wikis_dumps(&client).await.unwrap();
-    let dumpwikis_set: BTreeSet<String> = BTreeSet::from_iter(dumpwikis);
-    println!("Wikis (currently) dumped but not in Wikidata:");
-    for x in dumpwikis_set.difference(&wdwikis_set) {
-        println!("* {}", x);
-    }
-    println!();
-    println!("Wikis in Wikidata but not (currently) dumped:");
-    for x in wdwikis_set.difference(&dumpwikis_set) {
-        println!("* {}", x);
+    match matches.subcommand_name().unwrap() {
+        "list-wikis" => list_wikis().await?,
+        "list-types" => {
+            // todo: check args
+            let subcommand_matches = matches.subcommand_matches("list-types").unwrap();
+            list_types(
+                subcommand_matches.value_of("wiki name").unwrap(),
+                subcommand_matches.value_of("dump date").unwrap(),
+            )
+            .await?
+        }
+        _ => panic!("Unknown subcommand, should be caught by arg matching."),
     }
     Ok(())
 }
